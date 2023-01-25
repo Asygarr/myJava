fe5aa9b235998250dc2033b9252908568f048350
Asygarr <105841104721@student.unismuh.ac.id>
2023-01-25 21:51:22 +0800

    initial commit

--- src/latihan/helloWorld.java
+++ src/latihan/helloWorld.java
@@ -3,5 +3,9 @@ package latihan;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for (int i = 0; i < 3001; i++){
+            System.out.println("I Love You Nia");
+        }
     }
 }
