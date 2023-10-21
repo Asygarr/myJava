f4410313457f5d358a689b694ce668d927e1d2db
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-21 17:27:19 +0800

    Tugas Alpro 1 semester 5

--- src/semester5/TugasAlpro1.java
+++ src/semester5/TugasAlpro1.java
@@ -4,6 +4,13 @@ public class TugasAlpro1 {
 
     public static void main(String[] args) {
 
+        contoh1();
+        contoh2();
+        contoh3();
+
+    }
+
+    public static void contoh1() {
         // Contoh 1 analisis efektivitas algoritma dari segi ruang dan waktu dengan for
         int n = 10, total = 0, i; // ini dihitung 3 kali eksekusi karena ada 3 variabel
 
@@ -24,49 +31,63 @@ public class TugasAlpro1 {
         int rata2 = total2 / n2; // 1 kali eksekusi
 
         System.out.println("Rata-rata = " + rata2); // 1 kali eksekusi
+    }
+
+    public static void contoh2() {
+        int[] bilangan = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; // 1 kali eksekusi || panjang sebuah array = n (15)
 
         // Algotitma mencari bilangan prima dengan for
-        int[] bilangan = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; // 1 kali eksekusi
-
-        System.out.println("Bilangan prima dalam array:"); // 1 kali eksekusi
-
         for (int num : bilangan) {
-            boolean isPrime = true; // panjang sebuah array = n (15) kali eksekusi
+            boolean isPrime = true; // 1 x n = 15
 
-            for (int k = 2; k <= Math.sqrt(num); k++) { // n (15) kali eksekusi
-                if (num % k == 0) {
-                    isPrime = false; // kemungkinan terburuk = n (15) kali eksekusi
-                    break; // kemungkinan terburuk = n (15) kali eksekusi
+            // Operasi dasar
+            for (int k = 2; k <= Math.sqrt(num); k++) { // 1 x n = 15 || Operasi Aritmatika
+                if (num % k == 0) { // 1 x n = 15 || Operasi Aritmatika dan Perbandingan
+                    isPrime = false; // 1 x n = 15 || Operasi Penugasan
+                    break; // 1 x n = 15
                 }
             }
-
-            if (isPrime) {
-                System.out.print(num + " "); // kemungkinan terburuk = n (15) kali eksekusi
+            if (isPrime) { // 1 x n = 15 || Operasi Perbandingan
+                System.out.print(num + " "); // 1 x n = 15
             }
         }
 
-        // Algoritma mencari bilangan prima dengan do while
-        int[] bilangan2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; // 1 kali eksekusi
+        int[] bilangan2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}; // 1 kali eksekusi || panjang sebuah array = n (15)
         int l = 0; // 1 kali eksekusi
 
-        System.out.println("\nBilangan prima dalam array:"); // 1 kali eksekusi
-
+        // Algoritma mencari bilangan prima dengan do while
         do {
-            boolean isPrime = true; // panjang sebuah array = n (15) kali eksekusi
+            boolean isPrime = true; // 1 x n = 15
 
-            for (int j = 2; j <= Math.sqrt(bilangan2[l]); j++) { // n (15) kali eksekusi
-                if (bilangan2[l] % j == 0) {
-                    isPrime = false; // kemungkinan terburuk = n (15) kali eksekusi
-                    break; // kemungkinan terburuk = n (15) kali eksekusi
+            // Operasi dasar
+            for (int j = 2; j <= Math.sqrt(bilangan2[l]); j++) { // 1 x n = 15 || Operasi Aritmatika
+                if (bilangan2[l] % j == 0) { // 1 x n = 15 || Operasi Aritmatika dan Perbandingan
+                    isPrime = false; // 1 x n = 15 || Operasi Penugasan
+                    break; // 1 x n = 15
                 }
             }
-
-            if (isPrime) {
-                System.out.print(bilangan2[l] + " "); // kemungkinan terburuk = n (15) kali eksekusi
+            if (isPrime) { // 1 x n = 15 || Operasi Perbandingan
+                System.out.print(bilangan2[l] + " "); // 1 x n = 15
             }
 
-            l++; // n (10) kali eksekusi
-        } while (l < bilangan2.length); // n (10) kali eksekusi
+            l++; // 1 x n = 15
+        } while (l < bilangan2.length); // 1 x n = 15
+    }
 
+    public static void contoh3() {
+        int[] num = {1, 5, 3, 4, 2}; // angka terbesar berada pada index 1
+        int besar = 0;
+
+        // algoritma
+        for (int j : num) {
+
+            // operasi dasar di ekskusi hanya 2 kali
+            if (j > besar) { // operasi perbandingan
+                besar = j; // operasi penugasan
+            }
+
+        }
+
+        System.out.println("Bilangan terbesar = " + besar);
     }
 }
