c44b913426319c97ca915b64055d6d74f48d7014
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-21 17:27:53 +0800

    buat package karantina

---
+++ src/karantina/determinan.java
@@ -0,0 +1,26 @@
+package karantina;
+
+//buat matrix determinan
+public class determinan{
+    public static void main(String[] args) {
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int[][] b = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+        int[][] c = new int[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < b.length; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
